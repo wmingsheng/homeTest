97af41be01caefd53859f3be91130c78bd3ae64b
mingsheng <ipconfig_121@163.com>
2016-09-04 22:43:08 +0800

    first add Timer,TimerTask,schedule,RunTime

---
+++ Schedule.java
@@ -0,0 +1,17 @@
+import java.util.*;
+import java.lang.*;
+public class Schedule {
+	public static void main(String[] args) {
+		new Timer().schedule(new TimerTask() {
+				public void run() {
+					try {
+						Runtime.getRuntime().exec("cale.exe");
+					}
+					catch (Exception e) {
+						e.printStackTrace();
+					}
+				}
+			}
+			,2*10000);
+	}
+}
